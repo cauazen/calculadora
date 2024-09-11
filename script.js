7aa7813409dc3ef2265980117f4eabf25ddb09dd
Cauã F. Trapp <162443723+cauazen@users.noreply.github.com>
2024-09-11 09:22:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+var numero = ''
+var operadorAtual = ''
+var numeroAnterior = ''
+
 function addNumero(caractere){
-    document.getElementById('display').value += caractere
+    numero += caractere
+    document.getElementById('display').value = numero
 }
 
 function limparDisplay(){
+     numero = ''
+ operadorAtual = ''
+ numeroAnterior = ''
     document.getElementById('display').value = ''
 }
 
@@ -14,7 +22,38 @@ function apagar(){
 function operador(op){
     if(numero !== ''){
         operadorAtual = op
+        numeroAnterior = numero
+        numero = ''
         document.getElementById('display').value = ''
 
     }
+}
+
+
+function calcular(){
+    
+    let resultado = 0
+    let atual = parseFloat(numero)
+    let anterior = parseFloat(numeroAnterior)
+
+    if(operadorAtual === '+'){
+        resultado = anterior + atual
+    }else if(operadorAtual === '-'){
+        resultado = anterior - atual
+    }else if(operadorAtual === '*'){
+        resultado = anterior * atual
+    }else if(operadorAtual === '/'){
+        if(atual === 0){
+            document.getElementById('display').value = 'Erro: divisão por zero'
+            return
+        }
+        resultado = anterior/atual
+
+    }
+
+
+    document.getElementById('display').value = resultado
+    numero = resultado
+    operadorAtual = ''
+    numeroAnterior = ''
 }

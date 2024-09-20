af034fde9b8db84cee57d166ca76b4a88a5ac01f
Cauã F. Trapp <162443723+cauazen@users.noreply.github.com>
2024-09-20 08:19:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,58 +2,58 @@ var numero = ''
 var operadorAtual = ''
 var numeroAnterior = ''
 
-function addNumero(caractere){
+function addNumero(caractere) {
     numero += caractere
     document.getElementById('display').value = numero
 }
 
-function limparDisplay(){
-     numero = ''
- operadorAtual = ''
- numeroAnterior = ''
+function limparDisplay() {
+    numero = ''
+    operadorAtual = ''
+    numeroAnterior = ''
     document.getElementById('display').value = ''
 }
 
-function apagar(){
+function apagar() {
     let display = document.getElementById('display').value
-    document.getElementById('display').value = display.slice(0,-1)
+    document.getElementById('display').value = display.slice(0, -1)
+    numero = display.slice(0, -1) // Atualiza 'numero' após apagar
 }
 
-function operador(op){
-    if(numero !== ''){
+function operador(op) {
+    if (numero !== '') {
         operadorAtual = op
         numeroAnterior = numero
         numero = ''
         document.getElementById('display').value = ''
-
     }
 }
 
-
-function calcular(){
-    
+function calcular() {
     let resultado = 0
     let atual = parseFloat(numero)
     let anterior = parseFloat(numeroAnterior)
 
-    if(operadorAtual === '+'){
+    if (operadorAtual === '+') {
         resultado = anterior + atual
-    }else if(operadorAtual === '-'){
+    } else if (operadorAtual === '-') {
         resultado = anterior - atual
-    }else if(operadorAtual === '*'){
+    } else if (operadorAtual === '√') {
+        resultado = Math.sqrt(anterior) // Raiz quadrada do número anterior
+    } else if (operadorAtual === '%') {
+        resultado = (anterior * atual) / 100 // Porcentagem do número anterior
+    } else if (operadorAtual === '*') {
         resultado = anterior * atual
-    }else if(operadorAtual === '/'){
-        if(atual === 0){
+    } else if (operadorAtual === '/') {
+        if (atual === 0) {
             document.getElementById('display').value = 'Erro: divisão por zero'
             return
         }
-        resultado = anterior/atual
-
+        resultado = anterior / atual
     }
 
-
     document.getElementById('display').value = resultado
-    numero = resultado
+    numero = resultado.toString() // Converte resultado para string
     operadorAtual = ''
     numeroAnterior = ''
 }
